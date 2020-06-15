4ae21af91a71250cd6727eded3970f57a38bf253
romefar <vlad.romanovr@gmail.com>
2020-06-15 22:20:15 +0300

    Added backdrop

---
+++ client/src/shared/components/ui-elements/backdrop/backdrop-styles.js
@@ -0,0 +1,13 @@
+const styles = {
+  backdrop: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100vh',
+    background: 'rgba(0, 0, 0, 0.75)',
+    zIndex: 50
+  }
+};
+
+export default styles;
