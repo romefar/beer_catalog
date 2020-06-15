1f225ac767eedfe45dc4b91435ecf278e849c06f
romefar <vlad.romanovr@gmail.com>
2020-06-15 22:18:56 +0300

    Added header

---
+++ client/src/shared/components/navigation/header/header-syles.js
@@ -0,0 +1,12 @@
+const styles = {
+  header: {
+    height: '60px',
+    backgroundColor: 'rgba(0,0,0,0.9)',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 10px',
+    justifyContent: 'space-between'
+  }
+};
+
+export default styles;
---
+++ client/src/shared/components/navigation/header/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import withStyles from 'react-jss';
+import styles from './header-syles';
+import PropTypes from 'prop-types';
+
+const Header = ({ classes, children }) => {
+  return (
+    <header className={classes.header}>
+      {children}
+    </header>
+  );
+};
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  children: PropTypes.any
+};
+
+export default withStyles(styles)(Header);
