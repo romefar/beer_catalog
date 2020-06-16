68d3c8241758b34636221a134911c36e491595af
romefar <vlad.romanovr@gmail.com>
2020-06-16 20:11:22 +0300

    Added beer service

---
+++ client/src/services/beer-service.js
@@ -0,0 +1,23 @@
+import fetch from '../utils/fetch';
+
+class BeerService {
+  #beerRoute = '/beers?per_page=15';
+
+  fetchBeerItems = async () => {
+    return await fetch(this.#beerRoute);
+  }
+}
+
+const getBeerService = (() => {
+  let instance = null;
+  return () => {
+    if (instance) {
+      return instance;
+    } else {
+      instance = new BeerService();
+      return instance;
+    }
+  };
+})();
+
+export default getBeerService;
