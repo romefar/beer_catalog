8e1eba22a90a6406b577b9bd86ff37a7fbb08b16
romefar <vlad.romanovr@gmail.com>
2020-06-18 19:44:50 +0300

    Created input

---
+++ client/src/shared/components/form-elements/input/input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './input-styles';
+import withStyles from 'react-jss';
+
+const Input = (props) => {
+  const {
+    id,
+    label,
+    element,
+    placeholder,
+    validators,
+    initialValue,
+    errorText,
+    onInput
+  } = props;
+};
