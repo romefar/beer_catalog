989067d9e8917b98af4ffeb502aaf87e006fe06f
romefar <vlad.romanovr@gmail.com>
2020-06-18 19:43:54 +0300

    Added punk API support

--- client/src/services/beer-service.js
+++ client/src/services/beer-service.js
@@ -1,10 +1,50 @@
 import fetch from '../utils/fetch';
+import ResizeController from '../utils/resize-controller';
 
 class BeerService {
-  #beerRoute = '/beers?per_page=8';
+  #beerRoute = '/beers';
+  #params = {
+    per_page: 1,
+    page: 27
+  }
+
+  #defaultImageUrl = 'http://pluspng.com/img-png/beer-bottle-png-hd-a-beer-bottle-beer-bottle-brown-foam-free-png-and-psd-650.jpg';
+
+  #configureRequest = () => {
+    const itemsCount = ResizeController.getItemsCount();
+    this.#params.per_page = itemsCount;
+  }
+
+  #generateUrl = () => {
+    const url = [];
+    for (const [key, value] of Object.entries(this.#params)) {
+      if (value) {
+        url.push(`${key}=${value}`);
+      }
+    }
+    return `?${url.join('&')}`;
+  }
+
+  #replaceEmptyImages = (items) => {
+    const fixeditems = items.data.map(item => {
+      if (!item.image_url) {
+        item.image_url = this.#defaultImageUrl;
+      }
+      return item;
+    });
+    return {
+      ...items,
+      data: fixeditems
+    };
+  }
 
   fetchBeerItems = async () => {
-    return await fetch(this.#beerRoute);
+    this.#configureRequest();
+    const url = this.#generateUrl();
+    const rawItems = await fetch(`${this.#beerRoute}${url}`);
+    const items = this.#replaceEmptyImages(rawItems);
+    this.#params.page += 1;
+    return items;
   }
 }
 
