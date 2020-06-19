1c4cbe1481e4605c2bd69aefd502d16db3f63941
romefar <vlad.romanovr@gmail.com>
2020-06-19 19:50:56 +0300

    Added new actions

--- client/src/redux/actions/search-actions/search-actions.js
+++ client/src/redux/actions/search-actions/search-actions.js
@@ -1,12 +1,16 @@
 import {
   SEARCH_QUERY_CHANGE
 } from './search-actions-types';
+import {
+  beerListCleared
+} from '../beer-list-actions/beer-list-actions';
 
-const searchQueryChanged = (searchQuery) => {
-  return {
+const searchQueryChanged = (searchQuery) => (dispatch) => {
+  dispatch(beerListCleared());
+  dispatch({
     type: SEARCH_QUERY_CHANGE,
     payload: searchQuery
-  };
+  });
 };
 
 export {
