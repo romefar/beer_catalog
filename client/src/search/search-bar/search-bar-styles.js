d94b398f49555fe231d81105de74197f7f25bc5c
romefar <vlad.romanovr@gmail.com>
2020-06-19 19:48:17 +0300

    Updated input styles

--- client/src/search/search-bar/search-bar-styles.js
+++ client/src/search/search-bar/search-bar-styles.js
@@ -3,18 +3,31 @@ const styles = {
     width: '40vw',
     margin: '0 auto'
   },
+  searchForm: {
+    width: '100%',
+    margin: '20px 0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   inputField: {
     fontFamily: 'Lato, Arial, sans-serif',
     padding: '10px',
-    width: '100%',
     fontSize: '1rem',
     outline: 'none',
+    width: '90%',
     border: '1px solid #ccc',
     borderRadius: '5px',
+    margin: '0 3px 0 0',
     transition: 'all 0.5s ease-in',
-    margin: '20px 0',
     '&:hover, &:focus': {
       borderColor: '#696969'
+    }
+  },
+  inputFieldInvalid: {
+    borderColor: 'red',
+    '&:hover, &:focus': {
+      borderColor: 'red'
     }
   }
 };
