60bc1cbceaaf48aaeae4554008ee9c1b8fa461a6
romefar <vlad.romanovr@gmail.com>
2020-06-19 19:50:23 +0300

    Added beer list cleated action

--- client/src/redux/actions/beer-list-actions/beer-list-actions.js
+++ client/src/redux/actions/beer-list-actions/beer-list-actions.js
@@ -3,7 +3,8 @@ import getBeerService from '../../../services/beer-service';
 import {
   FETCH_BEER_LIST_REQUEST,
   FETCH_BEER_LIST_SUCCESS,
-  FETCH_BEER_LIST_FAILURE
+  FETCH_BEER_LIST_FAILURE,
+  BEER_LIST_CLEARED
 } from './beer-list-actions-types';
 
 const beerItemsRequested = () => {
@@ -26,16 +27,24 @@ const beerItemsLoadingFailed = (error) => {
   };
 };
 
-const fetchBeerItems = () => async (dispatch) => {
+const beerListCleared = () => {
+  return {
+    type: BEER_LIST_CLEARED
+  };
+};
+
+const fetchBeerItems = (query) => async (dispatch) => {
   try {
     dispatch(beerItemsRequested());
-    const beerItems = await getBeerService().fetchBeerItems();
+    const beerItems = await getBeerService().fetchBeerItems(query);
     dispatch(beerItemsLoaded(beerItems));
   } catch (error) {
+    console.log(error);
     dispatch(beerItemsLoadingFailed(error));
   }
 };
 
 export {
-  fetchBeerItems
+  fetchBeerItems,
+  beerListCleared
 };
