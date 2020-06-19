4086f231b523510143e2d4b36f92a32364f978d4
romefar <vlad.romanovr@gmail.com>
2020-06-19 19:49:00 +0300

    Added query validation & errorstyling

--- client/src/search/search-bar/search-bar.js
+++ client/src/search/search-bar/search-bar.js
@@ -5,23 +5,73 @@ import PropTypes from 'prop-types';
 import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
 import { searchQueryChanged } from '../../redux/actions/search-actions/search-actions';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import validateSearchQuery from '../../utils/validate-search-query';
+import clsx from 'clsx';
+import Tooltip from '@material-ui/core/Tooltip';
 
 class SearchBar extends PureComponent {
   state = {
-    query: ''
+    query: '',
+    isValid: true,
+    isTouched: false
   }
 
-  onChangeHandler =(e) => {
+  onChangeHandler = (e) => {
+    const value = e.target.value;
+    const [isValid] = validateSearchQuery(value);
     this.setState({
-      query: e.target.value
+      query: value,
+      isValid
     });
   }
 
+  onFocusHandler = () => {
+    this.setState({
+      isTouched: true
+    });
+  }
+
+  onSubmitHandler = (e) => {
+    const { isValid, query } = this.state;
+    const { searchQuery } = this.props;
+    e.preventDefault();
+    const [, validQuery] = validateSearchQuery(query);
+    console.log(isValid, validQuery, searchQuery);
+    if (isValid && validQuery !== searchQuery) {
+      this.props.searchQueryChanged(validQuery);
+    }
+  }
+
   render = () => {
-    const { classes, searchQuery, searchQueryChanged } = this.props;
+    const { classes } = this.props;
+    const { isValid, isTouched, query } = this.state;
     return (
       <div className={classes.inputContainer}>
-        <input className={classes.inputField} type='text' placeholder='Search query...' value={this.state.query} onChange={this.onChangeHandler}/>
+        <form className={classes.searchForm} onSubmit={this.onSubmitHandler}>
+          <Tooltip
+            title="Please, provide a valid beer name. For example 'Alphine Blue' "
+            arrow
+            open={!isValid && isTouched}
+          >
+            <input
+              className={clsx(
+                classes.inputField,
+                {
+                  [classes.inputFieldInvalid]: !isValid && isTouched
+                }
+              )}
+              type='text'
+              placeholder='Search query...'
+              value={query}
+              onChange={this.onChangeHandler}
+              onFocus={this.onFocusHandler}/>
+          </Tooltip>
+          <IconButton onClick={this.onSubmitHandler}>
+            <SearchIcon fontSize="large"/>
+          </IconButton>
+        </form>
       </div>
     );
   }
@@ -46,6 +96,6 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default compose(
-  withStyles(styles),
-  connect(mapStateToProps, mapDispatchToProps)
+  connect(mapStateToProps, mapDispatchToProps),
+  withStyles(styles)
 )(SearchBar);
