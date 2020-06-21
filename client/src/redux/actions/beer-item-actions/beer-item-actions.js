030be13a061de82fad3a5261128569b5374fd70f
romefar <vlad.romanovr@gmail.com>
2020-06-21 23:10:06 +0300

    Added beer item actions

---
+++ client/src/redux/actions/beer-item-actions/beer-item-actions.js
@@ -0,0 +1,42 @@
+import getBeerService from '../../../services/beer-service';
+
+import {
+  FETCH_BEER_ITEM_REQUEST,
+  FETCH_BEER_ITEM_SUCCESS,
+  FETCH_BEER_ITEM_FAILURE
+} from './beer-item-actions-types';
+
+const beerItemRequested = () => {
+  return {
+    type: FETCH_BEER_ITEM_REQUEST
+  };
+};
+
+const beerItemLoaded = (beerItem) => {
+  return {
+    type: FETCH_BEER_ITEM_SUCCESS,
+    payload: beerItem
+  };
+};
+
+const beerItemLoadingFailed = (error) => {
+  return {
+    type: FETCH_BEER_ITEM_FAILURE,
+    payload: error
+  };
+};
+
+const fetchSingleBeer = (options) => async (dispatch) => {
+  try {
+    dispatch(beerItemRequested());
+    const beerItem = await getBeerService().fetchSingleBeer(options);
+    dispatch(beerItemLoaded(beerItem));
+  } catch (error) {
+    console.log(error);
+    dispatch(beerItemLoadingFailed(error));
+  }
+};
+
+export {
+  fetchSingleBeer
+};
