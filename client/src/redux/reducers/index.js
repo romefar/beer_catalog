d753196fdfa87083c5d93f449bcfec53dc2b8ad7
romefar <vlad.romanovr@gmail.com>
2020-06-21 23:14:45 +0300

    Added new reducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import beerReducer from './beer-list-reducer';
 import searchReducer from './search-reducer';
+import beerItemReducer from './beer-item-reducer';
 
 export default combineReducers({
   beerList: beerReducer,
+  beerDetails: beerItemReducer,
   search: searchReducer
 });
