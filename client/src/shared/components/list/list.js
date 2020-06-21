396aa1bac9080667949c3eeec137551c3c0e0c9e
romefar <vlad.romanovr@gmail.com>
2020-06-21 23:02:40 +0300

    Added custom props support

--- client/src/shared/components/list/list.js
+++ client/src/shared/components/list/list.js
@@ -3,27 +3,40 @@ import withStyles from 'react-jss';
 import styles from './list-styles';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
+import clsx from 'clsx';
 
-const List = ({ items, classes }) => {
+const List = ({ items, classes, transparent, title }) => {
   return (
-    <ul className={classes.list}>
-      {items.map((item) => {
-        return (
-          <li
-            key={uuidv4()}
-            className={classes.listItem}
-          >
-            {item}
-          </li>
-        );
-      })}
-    </ul>
+    <div className={classes.container}>
+      <h3 className={classes.title}>{title}</h3>
+      <ul className={classes.list}>
+        {items.map((item) => {
+          return (
+            <li
+              key={uuidv4()}
+              className={clsx({
+                [classes.listItem]: true,
+                [classes.bordersTransparent]: transparent
+              })}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
 List.propTypes = {
   classes: PropTypes.object.isRequired,
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  transparent: PropTypes.bool
+};
+
+List.defaultProps = {
+  transparent: false
 };
 
 export default withStyles(styles)(List);
