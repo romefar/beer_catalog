e1c4c5319a06f2bc3e96d4743b7de540a06bd567
romefar <vlad.romanovr@gmail.com>
2020-06-21 23:02:08 +0300

    Fixed borders

--- client/src/shared/components/list/list-styles.js
+++ client/src/shared/components/list/list-styles.js
@@ -1,11 +1,37 @@
 const styles = {
   list: {
-    margin: 0
+    margin: 0,
+    padding: 0,
+    width: 'fit-content',
+    transition: 'background-color 0.5s ease-in'
+  },
+  container: {
+    padding: '0 12px',
+    fontFamily: 'Helvetica, Arial, sans-serif'
+  },
+  title: {
+    margin: '0 0 7px 0'
   },
   listItem: {
-    padding: '5px',
+    padding: '12px',
     border: '1px solid #ccc',
-    borderRadius: '5px'
+    listStyle: 'none',
+    '&:first-child': {
+      borderRadius: '5px 5px 0 0'
+    },
+    '&:nth-child(n + 1)': {
+      borderBottom: 'none'
+    },
+    '&:last-child': {
+      border: '1px solid #ccc',
+      borderRadius: '0 0 5px 5px'
+    },
+    '&:hover': {
+      backgroundColor: '#f5f5f5'
+    }
+  },
+  bordersTransparent: {
+    border: '1px solid transparent'
   }
 };
 export default styles;
