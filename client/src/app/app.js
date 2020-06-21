5dce04ef5f2f156cc837e048eea5a1eb4fb28fef
romefar <vlad.romanovr@gmail.com>
2020-06-21 23:20:35 +0300

    Added route for beer detail

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './app-styles';
 import MainNavigation from '../shared/components/navigation/main-navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import withStyles, { ThemeProvider } from 'react-jss';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import BeerContainer from '../beer/containers/beer-container';
+import BeerDetailsContainer from '../beer/containers/beer-details-container';
 const theme = {
   linkColor: 'white'
 };
@@ -16,8 +17,10 @@ const App = (props) => {
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <MainNavigation />
-          {/* <LoadingSpinner /> */}
-          <BeerContainer />
+          <Route path="/" exact>
+            <BeerContainer />
+          </Route>
+          <Route path="/beer/:beerId" component={BeerDetailsContainer} />
         </ThemeProvider>
       </Provider>
     </Router>
