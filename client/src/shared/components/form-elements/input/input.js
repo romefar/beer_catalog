e7f9f48c18842e76948061d78ee1bc620d4f0fa8
romefar <vlad.romanovr@gmail.com>
2020-06-22 20:27:58 +0300

    Added input types

--- client/src/shared/components/form-elements/input/input.js
+++ client/src/shared/components/form-elements/input/input.js
@@ -1,16 +1,83 @@
 import React from 'react';
 import styles from './input-styles';
 import withStyles from 'react-jss';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 const Input = (props) => {
   const {
-    id,
     label,
-    element,
-    placeholder,
-    validators,
-    initialValue,
-    errorText,
+    classes,
+    elementType,
+    value,
+    isValid,
+    isTouched,
+    errorMsg,
+    config,
     onInput
   } = props;
+
+  let element = null;
+  switch (elementType) {
+    case 'input':
+      element = <input
+        {...config}
+        className={clsx({
+          [classes.inputItem]: true,
+          [classes.inputInvalid]: !isValid && isTouched
+        })}
+        value={value}
+        onChange={onInput}
+      />;
+      break;
+    case 'textarea':
+      element = <textarea
+        {...config}
+        className={clsx({
+          [classes.inputItem]: true,
+          [classes.inputInvalid]: !isValid && isTouched
+        })}
+        value={value}
+        onChange={onInput}
+      />;
+      break;
+    default:
+      element = <input
+        {...config}
+        className={clsx({
+          [classes.inputItem]: true,
+          [classes.inputInvalid]: !isValid && isTouched
+        })}
+        value={value}
+        onChange={onInput}
+      />;
+      break;
+  }
+
+  return (
+    <div className={classes.container}>
+      <label className={classes.labelItem}>{label}</label>
+      {element}
+      {/* <VisibilityOutlinedIcon /> */}
+      {!isValid && isTouched && <p className={classes.errorMessage}>{errorMsg}</p>}
+    </div>
+  );
 };
+
+Input.propTypes = {
+  isValid: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  value: PropTypes.string.isRequired,
+  elementType: PropTypes.string,
+  isTouched: PropTypes.bool.isRequired,
+  errorMsg: PropTypes.string,
+  config: PropTypes.object.isRequired,
+  onInput: PropTypes.func.isRequired
+};
+
+Input.defaultProps = {
+  errorMsg: 'Invalid input'
+};
+
+export default withStyles(styles)(Input);
