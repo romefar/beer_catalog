8b47216ed120d3aff4fdcdd403165265a12d0586
romefar <vlad.romanovr@gmail.com>
2020-06-23 19:49:48 +0300

    Added profile panel styles

---
+++ client/src/profile/components/profile-panel/profile-panel-styles.js
@@ -0,0 +1,28 @@
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    position: 'relative',
+    color: 'white',
+    '&:hover img': {
+      borderColor: 'rgb(169, 112, 255);'
+    }
+  },
+  profileImage: {
+    width: '50px'
+  },
+  userName: {
+    margin: '0 7px',
+    fontFamily: 'Lato, Arial, sans-serif'
+  },
+  expandIcon: {
+    transition: 'transform 0.5s ease-in'
+  },
+  expandIconOpened: {
+    transform: 'rotate(180deg)'
+  }
+};
+
+export default styles;
