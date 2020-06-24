f6df70a9376be714014c7bd2842c23b2118b20cc
romefar <vlad.romanovr@gmail.com>
2020-06-24 21:07:45 +0300

    Added user model

---
+++ server/src/db/models/user.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  image: {
+    type: String
+  }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = model('User', userSchema);
