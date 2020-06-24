08891f4e88ede7868501af0e05afed7da5235290
romefar <vlad.romanovr@gmail.com>
2020-06-24 21:09:51 +0300

    Added base repository

---
+++ server/src/api/data-access-layer/base-repository.js
@@ -0,0 +1,35 @@
+class BaseRepository {
+  constructor (model) {
+    this.model = model;
+  }
+
+  getAll = async (excludeFields) => {
+    return await this.model.find({}, excludeFields);
+  }
+
+  getOneByCriteria = async (options) => {
+    return await this.model.findOne(options);
+  }
+
+  getManyByCriteria = async (options) => {
+    return await this.model.find(options);
+  }
+
+  getById = async (id) => {
+    return await this.model.findById(id);
+  }
+
+  delete = async (options) => {
+    return await this.model.findOneAndDelete(options);
+  }
+
+  create = async (data) => {
+    return await this.model.create(data);
+  }
+
+  update = async (conditionObject, updateObject) => {
+    return await this.model.findOneAndUpdate(conditionObject, updateObject);
+  }
+}
+
+module.exports = BaseRepository;
