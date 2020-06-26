1570b5f3ab4dd67966c950a4e014139d72f78d85
romefar <vlad.romanovr@gmail.com>
2020-06-26 20:58:04 +0300

    Added auth

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './app-styles';
 import MainNavigation from '../shared/components/navigation/main-navigation';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import withStyles, { ThemeProvider } from 'react-jss';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+import { connect } from 'react-redux';
+import { bindActionCreators, compose } from 'redux';
 import BeerContainer from '../beer/containers/beer-container';
 import BeerDetailsContainer from '../beer/containers/beer-details-container';
-import SignUp from '../authorization/components/sign-up';
-import SignIn from '../authorization/components/sign-in';
+import SignUpContainer from '../authorization/containers/sign-up-container';
+import SignInContainer from '../authorization/containers/sign-in-container';
+import { logout, signInSuccess } from '../redux/actions/sign-in-actions/sign-in-actions';
+import PropTypes from 'prop-types';
 const theme = {
   linkColor: 'white'
 };
 
-const App = (props) => {
-  return (
-    <Router>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <MainNavigation />
-          <Route path="/" exact>
-            <BeerContainer />
-          </Route>
-          <Route path="/beer/:beerId" component={BeerDetailsContainer} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/signin" component={SignIn} />
-        </ThemeProvider>
-      </Provider>
-    </Router>
-  );
+let timerId;
+
+class App extends Component {
+  #logoutTokenHandler = (expiration, logout) => {
+    timerId = setTimeout(logout, expiration.getTime() - new Date().getTime());
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (this.props.isLoggedIn && !timerId) {
+      timerId = this.#logoutTokenHandler(this.props.userData.expiration, this.props.logout);
+    }
+  }
+
+  componentDidMount = () => {
+    const storedData = JSON.parse(localStorage.getItem('AUTH_TOKEN'));
+    if (storedData && new Date(storedData.expiration > new Date())) {
+      this.props.signInSuccess({
+        ...storedData,
+        expiration: new Date(storedData.expiration)
+      });
+    }
+  }
+
+  componentWillUnmount = () => {
+    clearTimeout(timerId);
+  }
+
+  render = () => {
+    const { isLoggedIn } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <MainNavigation />
+        <Route path="/" exact>
+          <BeerContainer />
+        </Route>
+        <Route path="/beer/:beerId" component={BeerDetailsContainer} />
+        {!isLoggedIn && <Route path="/signup" component={SignUpContainer} />}
+        {!isLoggedIn && <Route path="/signin" component={SignInContainer} />}
+      </ThemeProvider>
+    );
+  }
 };
 
-export default withStyles(styles)(App);
+const mapStateToProps = ({
+  signIn: { isLoggedIn, hasError, userData }
+}) => {
+  return {
+    isLoggedIn,
+    userData
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    logout,
+    signInSuccess
+  }, dispatch);
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  userData: PropTypes.object,
+  logout: PropTypes.func.isRequired,
+  signInSuccess: PropTypes.func.isRequired
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withStyles(styles)
+)(App);
