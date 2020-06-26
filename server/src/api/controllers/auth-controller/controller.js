6b992c5c9fe189aaa7864114dc65fa48f236c45e
romefar <vlad.romanovr@gmail.com>
2020-06-26 21:07:26 +0300

    Fixed response object

--- server/src/api/controllers/auth-controller/controller.js
+++ server/src/api/controllers/auth-controller/controller.js
@@ -3,8 +3,8 @@ const authService = require('../../services/auth-service');
 class AuthController {
   signIn = async (req, res, next) => {
     try {
-      const { userId, image, token } = await authService.signIn(req.body);
-      res.send({ userId, token, image });
+      const { userId, image, userName, token } = await authService.signIn(req.body);
+      res.send({ userId, token, userName, image });
     } catch (error) {
       next(error);
     }
