d5ec6e3c9e05c4e7744c13b23b91a49d024e94b4
romefar <vlad.romanovr@gmail.com>
2020-06-27 20:15:30 +0300

    Added token

--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -2,11 +2,18 @@ import axios from './api';
 
 const fetch = async (url, method = 'GET', body = null, headers = {}, params = {}) => {
   try {
+    let authHeader = {};
+    if (localStorage.getItem('AUTH_TOKEN')) {
+      const token = JSON.parse(localStorage.getItem('AUTH_TOKEN')).token;
+      authHeader = {
+        Authorization: `Bearer ${token}`
+      };
+    }
     const items = await axios({
       url,
       method,
       data: body,
-      headers,
+      headers: { ...headers, ...authHeader },
       params
     });
     return items.data;
