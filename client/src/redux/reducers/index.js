552306fbced3e1b0f3d7dbdbd2dfa5d12f2c33d6
romefar <vlad.romanovr@gmail.com>
2020-06-28 20:02:51 +0300

    Updated imports

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -5,10 +5,12 @@ import beerItemReducer from './beer-item-reducer';
 import signInReducer from './sign-in-reducer';
 import signUpReducer from './sign-up-reducer';
 import profileReducer from './profile-reducer';
+import beerFavouritesReducer from './beer-favourites-reducer';
 
 export default combineReducers({
   beerList: beerReducer,
   beerDetails: beerItemReducer,
+  beerFavourites: beerFavouritesReducer,
   profile: profileReducer,
   search: searchReducer,
   signIn: signInReducer,
