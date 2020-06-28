9332d0db42b32e716af86019676a24492b565752
romefar <vlad.romanovr@gmail.com>
2020-06-28 19:58:30 +0300

    Added sign up reset prop

--- client/src/authorization/containers/sign-up-container.js
+++ client/src/authorization/containers/sign-up-container.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { signUpSubmit } from '../../redux/actions/sign-up-actions/sign-up-actions';
+import { signUpSubmit, signUpReset } from '../../redux/actions/sign-up-actions/sign-up-actions';
 import SignUp from '../components/sign-up';
+import PropTypes from 'prop-types';
 
 class SignUpContainer extends Component {
   render = () => {
-    const { hasError, isRegistered, signUpSubmit, history } = this.props;
+    const { hasError, isRegistered, signUpSubmit, history, signUpReset } = this.props;
     return (
       <SignUp
         hasError={hasError}
         isRegistered={isRegistered}
+        signUpReset={signUpReset}
         onSubmit={signUpSubmit}
         history={history}
       />
@@ -29,8 +31,17 @@ const mapStateToProps = ({
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
-    signUpSubmit
+    signUpSubmit,
+    signUpReset
   }, dispatch);
+};
+
+SignUpContainer.propTypes = {
+  isRegistered: PropTypes.bool.isRequired,
+  signUpSubmit: PropTypes.func.isRequired,
+  signUpReset: PropTypes.func.isRequired,
+  hasError: PropTypes.object,
+  history: PropTypes.object.isRequired
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignUpContainer);
