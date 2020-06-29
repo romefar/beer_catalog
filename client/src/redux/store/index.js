8216acc6e02ba7b685b51183011496257302e926
romefar <vlad.romanovr@gmail.com>
2020-06-29 20:58:27 +0300

    Added socket middleware

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
+import socketMiddleware from '../middleware/socketMiddleware';
+import socketService from '../../services/socket-service';
 
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, socketMiddleware(socketService)),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
