a44b4b926c6a4d1ef05125359bdcb630f1c1b558
romefar <vlad.romanovr@gmail.com>
2020-06-30 23:02:18 +0300

    Updated comments reducer

--- client/src/redux/reducers/comments-reducer.js
+++ client/src/redux/reducers/comments-reducer.js
@@ -2,42 +2,75 @@ import {
   DATA_SEND_REQUEST,
   DATA_SEND_SUCCESS,
   DATA_SEND_FAILURE,
+  DATA_LOAD_INITIAL_COMMENTS_REQUEST,
+  DATA_LOAD_INITIAL_COMMENTS_SUCCESS,
+  DATA_LOAD_INITIAL_COMMENTS_FAILURE,
   SOCKET_CONNECT_REQUEST,
   SOCKET_CONNECT_FAILURE,
   SOCKET_CONNECT_SUCCESS,
   SOCKET_DISCONNECT_REQUEST,
   SOCKET_DISCONNECT_FAILURE,
   SOCKET_DISCONNECT_SUCCESS,
-  DATA_UPDATE_COMMENTS_REQUEST,
-  DATA_UPDATE_COMMENTS_SUCCESS,
-  DATA_UPDATE_COMMENTS_FAILURE
-} from '../actions/comments-actions/comment-actions-types';
+  UPDATE_COMMENTS_SUCCESS,
+  UPDATE_COMMENTS_FAILURE,
+  SET_UPDATE_COMMENTS_HANDLER_SUCCESS,
+  SET_UPDATE_COMMENTS_HANDLER_FAILURE,
+  SHOW_NEW_COMMENTS,
+  DELETE_COMMENT_SUCCESS,
+  DELETE_COMMENT_FAILURE
+} from '../actions/comments-actions/comments-actions-types';
 
 const initalState = {
   isSending: false,
   hasError: null,
+  isLoading: false,
   connected: false,
+  newCommentsHandlerSettled: false,
   items: [],
-  hasNewComments: false
+  newComments: [],
+  hasNewComments: false,
+  isInitialLoad: true
 };
 
 const commentsReducer = (state = initalState, action) => {
   switch (action.type) {
-    case DATA_UPDATE_COMMENTS_REQUEST:
     case SOCKET_CONNECT_REQUEST:
     case SOCKET_DISCONNECT_REQUEST:
       return {
         ...state
       };
-    case DATA_UPDATE_COMMENTS_SUCCESS:
+    case SHOW_NEW_COMMENTS:
       return {
         ...state,
-        items: [...state.items, ...[action.payload]],
+        items: [...state.items, ...state.newComments],
+        hasNewComments: false
+      };
+    case DATA_LOAD_INITIAL_COMMENTS_REQUEST:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case DELETE_COMMENT_SUCCESS:
+      return {
+        ...state,
+        items: state.items.filter(item => item._id !== action.payload)
+      };
+    case SET_UPDATE_COMMENTS_HANDLER_SUCCESS:
+      return {
+        ...state,
+        newCommentsHandlerSettled: true
+      };
+    case UPDATE_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        newComments: [...state.newComments, ...[action.payload]],
         hasNewComments: true
       };
-    case DATA_UPDATE_COMMENTS_FAILURE:
+    case UPDATE_COMMENTS_FAILURE:
     case SOCKET_CONNECT_FAILURE:
     case SOCKET_DISCONNECT_FAILURE:
+    case DELETE_COMMENT_FAILURE:
+    case SET_UPDATE_COMMENTS_HANDLER_FAILURE:
       return {
         ...state,
         hasNewComments: false,
@@ -49,12 +82,20 @@ const commentsReducer = (state = initalState, action) => {
         hasError: null,
         connected: true
       };
-    case SOCKET_DISCONNECT_SUCCESS:
+    case DATA_LOAD_INITIAL_COMMENTS_SUCCESS:
       return {
-        isSending: false,
-        hasError: null,
-        connected: false
+        ...state,
+        items: [...action.payload],
+        isLoading: false,
+        isInitialLoad: false
       };
+    case DATA_LOAD_INITIAL_COMMENTS_FAILURE:
+      return {
+        ...state,
+        hasError: action.error
+      };
+    case SOCKET_DISCONNECT_SUCCESS:
+      return initalState;
     case DATA_SEND_REQUEST:
       return {
         ...state,
