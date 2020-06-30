1acfe522fa2aeb33aaf4adaf0394b84c2b0722a9
romefar <vlad.romanovr@gmail.com>
2020-06-30 23:06:25 +0300

    Added socket's handlers

--- server/src/api/controllers/comments-controller/controller.js
+++ server/src/api/controllers/comments-controller/controller.js
@@ -1,42 +1,57 @@
+const commentsService = require('../../services/comments-service');
 const CONNECTION = 'connection';
 const DISCONNECTION = 'disconnect';
 const SEND_MESSAGE = 'sendMessage';
-const GET_NEW_COMMENTS = 'getNewComments';
-
-const comments = [{
-  id: 1,
-  name: 'Mark',
-  description: 'asdasdasd'
-}, {
-  id: 2,
-  name: 'Jen',
-  description: 'asdasd2asd'
-}, {
-  id: 3,
-  name: 'Joseph',
-  description: 'asdasdassd'
-}];
+const BROADCAST_NEW_COMMENT = 'getNewComments';
+const GET_INITIAL_COMMENTS = 'getInitialComments';
+const DELETE_COMMENT = 'deleteComment';
 
 class CommentsController {
   constructor (io) {
     this.io = io;
   }
 
+  getInitialComments = async (id, cb) => {
+    try {
+      const comments = await commentsService.getInitialComments(id);
+      cb(comments.comments);
+    } catch (error) {
+      // eslint-disable-next-line standard/no-callback-literal
+      cb({ error: error.message });
+    }
+  }
+
+  deleteComment = async (data, cb) => {
+    try {
+      await commentsService.deleteComment(data.commentId, data.beerId);
+      cb(data.commentId);
+    } catch (error) {
+      // eslint-disable-next-line standard/no-callback-literal
+      cb({ error: error.message });
+    }
+  }
+
+  addNewComment = async (data, cb) => {
+    try {
+      const comment = await commentsService.addNewComment(data.id, { description: data.description, creatorId: data.creatorId });
+      // eslint-disable-next-line standard/no-callback-literal
+      cb({});
+      this.io.emit(BROADCAST_NEW_COMMENT, comment);
+    } catch (error) {
+      // eslint-disable-next-line standard/no-callback-literal
+      cb({ error: error.message });
+    }
+  }
+
   listen = () => {
     this.io.on(CONNECTION, (socket) => {
       console.log('a user is connected ' + socket.id);
 
-      socket.on(SEND_MESSAGE, (data, cb) => {
-        console.log(data);
-        comments.push(data);
-        // cb({});
-        socket.emit('getNewComments', data);
-      });
+      this.socket = socket;
 
-      socket.on(GET_NEW_COMMENTS, (data, cb) => {
-
-      });
-
+      socket.on(SEND_MESSAGE, this.addNewComment);
+      socket.on(GET_INITIAL_COMMENTS, this.getInitialComments);
+      socket.on(DELETE_COMMENT, this.deleteComment);
       socket.on(DISCONNECTION, () => {
         console.log('a user is disconnected ' + socket.id);
       });
