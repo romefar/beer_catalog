f2ecba1b4e888182e264d6f005af6860b007911a
romefar <vlad.romanovr@gmail.com>
2020-06-30 22:59:30 +0300

    Implemented basic methods with socket

--- client/src/comments/containers/comments-container.js
+++ client/src/comments/containers/comments-container.js
@@ -1,23 +1,59 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { socketSendMessage, socketConnect, socketDisconnect, socketGetNewMessages } from '../../redux/actions/comments-actions/comments-actions';
+import {
+  socketSendMessage,
+  socketConnect,
+  socketDisconnect,
+  socketSetNewMessageHandler,
+  socketLoadInitialComments,
+  getNewComment,
+  socketDeleteComment,
+  showNewComments
+} from '../../redux/actions/comments-actions/comments-actions';
 import CommentForm from '../components/comment-form';
+import CommentsList from '../components/comments-list';
+import PropTypes from 'prop-types';
 
 class CommentsContainer extends Component {
-  #socket;
+  state = {
+    isModalVisible: false,
+    commentId: null
+  }
 
   componentDidMount = () => {
     this.props.socketConnect();
-    this.props.socketGetNewMessages(this.onChangeHandler);
+    this.props.socketSetNewMessageHandler(this.props.getNewComment);
   }
 
-  onChangeHandler = (obj) => {
-    console.log(obj)
+  componentDidUpdate = () => {
+    if (this.props.connected && !this.props.isLoading && this.props.isInitialLoad && !this.props.hasError) {
+      this.props.socketLoadInitialComments(this.props.id);
+    }
+  }
+
+  deleteCommentHandler = () => {
+    this.props.socketDeleteComment(this.state.commentId, this.props.id);
+    this.setState({
+      isModalVisible: false
+    });
+  }
+
+  showModalHandler = (id) => {
+    this.setState({
+      isModalVisible: true,
+      commentId: id
+    });
+  }
+
+  closeModalHandler = () => {
+    this.setState({
+      isModalVisible: false
+    });
   }
 
   onClickHandler = () => {
-    this.props.socketSendMessage({ id: 5, name: 'John', description: '888888'});
+    this.props.showNewComments();
   }
 
   componentWillUnmount = () => {
@@ -25,25 +61,46 @@ class CommentsContainer extends Component {
   }
 
   render = () => {
+    const { isLoading, hasError, userData, isLoggedIn, items, hasNewComments } = this.props;
     return (
       <Fragment>
-        <p>Hello</p>
-        <button onClick={this.onClickHandler}>
-        Click on me
-        </button>
-        <CommentForm hasError={this.props.hasError}/>
+        {isLoggedIn &&
+         <CommentForm
+           hasError={this.props.hasError}
+           onSubmit={this.props.socketSendMessage}
+           id={this.props.id}
+         />}
+        <CommentsList
+          isLoading={isLoading}
+          isLoggedIn={isLoggedIn}
+          items={items}
+          isModalVisible={this.state.isModalVisible}
+          userData={userData}
+          hasNewComments={hasNewComments}
+          hasError={hasError}
+          showModalHandler={this.showModalHandler}
+          closeModalHandler={this.closeModalHandler}
+          deleteCommentHandler={this.deleteCommentHandler}
+          onUpdateClick={this.onClickHandler}
+        />
       </Fragment>
     );
   }
 }
 
 const mapStateToProps = ({
-  comments: { isSending, hasError, connected }
+  comments: { hasError, items, isLoading, hasNewComments, connected, isInitialLoad },
+  signIn: { userData, isLoggedIn }
 }) => {
   return {
-    isSending,
+    items,
+    userData,
+    isLoading,
+    isLoggedIn,
+    hasNewComments,
     hasError,
-    connected
+    connected,
+    isInitialLoad
   };
 };
 
@@ -51,9 +108,33 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     socketSendMessage,
     socketConnect,
-    socketGetNewMessages,
-    socketDisconnect
+    socketSetNewMessageHandler,
+    socketLoadInitialComments,
+    socketDeleteComment,
+    socketDisconnect,
+    getNewComment,
+    showNewComments
   }, dispatch);
+};
+
+CommentsContainer.propTypes = {
+  id: PropTypes.number.isRequired,
+  userData: PropTypes.object,
+  items: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  isInitialLoad: PropTypes.bool.isRequired,
+  hasNewComments: PropTypes.bool.isRequired,
+  hasError: PropTypes.object,
+  connected: PropTypes.bool.isRequired,
+  socketSendMessage: PropTypes.func.isRequired,
+  socketConnect: PropTypes.func.isRequired,
+  socketSetNewMessageHandler: PropTypes.func.isRequired,
+  socketLoadInitialComments: PropTypes.func.isRequired,
+  socketDisconnect: PropTypes.func.isRequired,
+  socketDeleteComment: PropTypes.func.isRequired,
+  getNewComment: PropTypes.func.isRequired,
+  showNewComments: PropTypes.func.isRequired
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentsContainer);
