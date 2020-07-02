c8d95af8a71fffeeed40247d8c15b4e3e8e3e8e5
romefar <vlad.romanovr@gmail.com>
2020-07-02 21:27:28 +0300

    Updated reducer

--- client/src/redux/reducers/profile-reducer.js
+++ client/src/redux/reducers/profile-reducer.js
@@ -5,12 +5,21 @@ import {
   REMOVE_BEER_ID_FROM_FAVOURITES_FAILURE,
   FETCH_BEER_IDS_LIST_FAVOURITES_SUCCESS,
   FETCH_BEER_IDS_LIST_FAVOURITES_FAILURE,
-  IMAGE_UPLOAD_FAILURE
+  IMAGE_UPLOAD_REQUEST,
+  PASSWORD_CHANGE_REQUEST,
+  IMAGE_UPLOAD_SUCCESS,
+  PASSWORD_CHANGE_SUCCESS,
+  IMAGE_UPLOAD_FAILURE,
+  PASSWORD_CHANGE_FAILURE,
+  DELETE_PROFILE_REQUEST,
+  DELETE_PROFILE_SUCCESS,
+  DELETE_PROFILE_FAILURE
 } from '../actions/profile-actions/profile-actions-types';
 
 const initialState = {
   favourites: [],
-  hasError: null
+  hasError: null,
+  actionCompletedSuccessfully: false
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -19,14 +28,34 @@ const profileReducer = (state = initialState, action) => {
     case FETCH_BEER_IDS_LIST_FAVOURITES_SUCCESS:
     case REMOVE_BEER_ID_FROM_FAVOURITES_SUCCESS:
       return {
+        ...state,
         hasError: null,
         favourites: [...action.payload]
+      };
+    case IMAGE_UPLOAD_REQUEST:
+    case PASSWORD_CHANGE_REQUEST:
+    case DELETE_PROFILE_REQUEST:
+      return {
+        ...state,
+        actionCompletedSuccessfully: false
+      };
+    case IMAGE_UPLOAD_SUCCESS:
+    case PASSWORD_CHANGE_SUCCESS:
+    case DELETE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        hasError: null,
+        actionCompletedSuccessfully: true
       };
     case ADD_BEER_ID_TO_FAVOURITES_FAILURE:
     case REMOVE_BEER_ID_FROM_FAVOURITES_FAILURE:
     case FETCH_BEER_IDS_LIST_FAVOURITES_FAILURE:
+    case PASSWORD_CHANGE_FAILURE:
     case IMAGE_UPLOAD_FAILURE:
+    case DELETE_PROFILE_FAILURE:
       return {
+        ...state,
+        actionCompletedSuccessfully: false,
         hasError: action.payload,
         favourites: []
       };
