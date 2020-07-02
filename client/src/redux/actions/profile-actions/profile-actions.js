e0928d47d9ac5277ea9cb6f85956e3bcd3b18fb9
romefar <vlad.romanovr@gmail.com>
2020-07-02 21:26:54 +0300

    Added actions for delete profile/change password

--- client/src/redux/actions/profile-actions/profile-actions.js
+++ client/src/redux/actions/profile-actions/profile-actions.js
@@ -6,11 +6,18 @@ import {
   REMOVE_BEER_ID_FROM_FAVOURITES_FAILURE,
   FETCH_BEER_IDS_LIST_FAVOURITES_SUCCESS,
   FETCH_BEER_IDS_LIST_FAVOURITES_FAILURE,
+  IMAGE_UPLOAD_REQUEST,
   IMAGE_UPLOAD_SUCCESS,
-  IMAGE_UPLOAD_FAILURE
+  IMAGE_UPLOAD_FAILURE,
+  PASSWORD_CHANGE_REQUEST,
+  PASSWORD_CHANGE_SUCCESS,
+  PASSWORD_CHANGE_FAILURE,
+  DELETE_PROFILE_REQUEST,
+  DELETE_PROFILE_SUCCESS,
+  DELETE_PROFILE_FAILURE
 } from './profile-actions-types';
 
-import { updateSignInImage } from '../sign-in-actions/sign-in-actions';
+import { updateSignInImage, logout } from '../sign-in-actions/sign-in-actions';
 
 const fetchBeerFavouritesIds = () => async (dispatch) => {
   try {
@@ -83,6 +90,9 @@ const removeBeerFromFavouritesFailure = (error) => {
 
 const updateProfileImage = (formData) => async (dispatch) => {
   try {
+    dispatch({
+      type: IMAGE_UPLOAD_REQUEST
+    });
     const imageData = await getProfileService().updateProfileImage(formData);
     dispatch({
       type: IMAGE_UPLOAD_SUCCESS
@@ -96,9 +106,46 @@ const updateProfileImage = (formData) => async (dispatch) => {
   }
 };
 
+const changeProfilePassword = (formData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PASSWORD_CHANGE_REQUEST
+    });
+    await getProfileService().changeProfilePassword(formData);
+    dispatch({
+      type: PASSWORD_CHANGE_SUCCESS
+    });
+  } catch (error) {
+    dispatch({
+      type: PASSWORD_CHANGE_FAILURE,
+      payload: error
+    });
+  }
+};
+
+const deleteProfile = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: DELETE_PROFILE_REQUEST
+    });
+    await getProfileService().deleteProfile();
+    dispatch({
+      type: DELETE_PROFILE_SUCCESS
+    });
+    dispatch(logout());
+  } catch (error) {
+    dispatch({
+      type: DELETE_PROFILE_FAILURE,
+      payload: error
+    });
+  }
+};
+
 export {
   addBeerToFavourites,
   removeBeerFromFavourites,
   fetchBeerFavouritesIds,
-  updateProfileImage
+  updateProfileImage,
+  changeProfilePassword,
+  deleteProfile
 };
