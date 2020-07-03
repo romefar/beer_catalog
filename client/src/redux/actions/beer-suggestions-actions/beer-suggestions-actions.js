f2e10aae8a56acc7b2a4dd33c925c467de02b991
romefar <vlad.romanovr@gmail.com>
2020-07-03 22:28:28 +0300

    Add beer suggestions actions

---
+++ client/src/redux/actions/beer-suggestions-actions/beer-suggestions-actions.js
@@ -0,0 +1,40 @@
+import getBeerService from '../../../services/beer-service';
+import {
+  FETCH_BEER_SUGGESTIONS_REQUEST,
+  FETCH_BEER_SUGGESTIONS_SUCCESS,
+  FETCH_BEER_SUGGESTIONS_FAILURE
+} from './beer-suggestions-actions-types';
+
+const fetchBeerItemsRequest = () => {
+  return {
+    type: FETCH_BEER_SUGGESTIONS_REQUEST
+  };
+};
+
+const fetchBeerItemsSuccess = (beerItems) => {
+  return {
+    type: FETCH_BEER_SUGGESTIONS_SUCCESS,
+    payload: beerItems
+  };
+};
+
+const fetchBeerItemsFailure = (error) => {
+  return {
+    type: FETCH_BEER_SUGGESTIONS_FAILURE,
+    payload: error
+  };
+};
+
+const fetchBeerSuggestions = (page) => async (dispatch) => {
+  try {
+    dispatch(fetchBeerItemsRequest());
+    const beerItems = await getBeerService().fetchBeerSuggestions(page);
+    dispatch(fetchBeerItemsSuccess(beerItems));
+  } catch (error) {
+    dispatch(fetchBeerItemsFailure(error));
+  }
+};
+
+export {
+  fetchBeerSuggestions
+};
