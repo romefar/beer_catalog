41bd8eb3a5613aa77671380ae1e3e52703a64f24
romefar <vlad.romanovr@gmail.com>
2020-07-05 22:41:19 +0300

    Removed console logs

--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -18,8 +18,6 @@ const fetch = async (url, method = 'GET', body = null, headers = {}, params = {}
     });
     return items.data;
   } catch (error) {
-    console.dir(error);
-    console.log(error.response.data.message);
     throw new Error(error.response.data.message);
   }
 };
