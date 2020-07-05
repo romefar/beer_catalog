36a51db8e03d416fc6b0acf734b266822e827b66
romefar <vlad.romanovr@gmail.com>
2020-07-05 22:45:32 +0300

    Added logger && changed api endpoints names

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+const logger = require('./config/winston');
 const path = require('path');
+const cors = require('cors');
+
 const dbConnect = require('./db/connection/mongoose');
 const beerRoute = require('./api/controllers/beer-controller/routes');
 const authRoute = require('./api/controllers/auth-controller/routes');
 const ratingRoute = require('./api/controllers/rating-controller/routes');
 const profileRoute = require('./api/controllers/profile-controller/routes');
 const auth = require('./middlewares/auth');
+
 const app = express();
+
 const server = require('http').Server(app);
 const initSocket = require('./socket/initSocket');
 const socket = initSocket(server);
@@ -22,15 +26,22 @@ app.use(cors({
   methods: 'GET, POST, PATCH, DELETE'
 }));
 
-app.use('/rating', ratingRoute);
-app.use('/beers', beerRoute);
-app.use('/profile', auth, profileRoute);
+app.use('/api/v1/rating', ratingRoute);
+app.use('/api/v1/beers', beerRoute);
+app.use('/api/v1/profile', auth, profileRoute);
 app.use(authRoute);
+
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
+  logger.error(`${error.code || 500} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
   res.status(error.code || 500).send({
     message: error.message || 'An unknown error occured.'
   });
@@ -40,10 +51,11 @@ const port = process.env.PORT || 5000;
 
 dbConnect()
   .then(() => {
+    logger.info('Succesfully connected to the database.');
     server.listen(port, () => {
-      console.log(`Server is running on port ${port}\nDB is running.`);
+      logger.info(`Server is running on port ${port}.`);
     });
   })
-  .catch(e => {
-    console.log(`An error has occured while running the db. ${e.message}`);
+  .catch(err => {
+    logger.error(`Couldn't connect to the database. - ${err.message}`);
   });
