4b64a26b71c8fb23cf99dd1e3c3ee5f163167a48
romefar <vlad.romanovr@gmail.com>
2020-09-19 17:29:48 +0300

    Added persist config

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,15 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducers from '../reducers';
 import socketMiddleware from '../middleware/socketMiddleware';
 import socketService from '../../services/socket-service';
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk, socketMiddleware(socketService))
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const persistConfig = {
+  key: 'br_catalog_user',
+  storage: storage,
+  whitelist: ['theme']
+};
 
-export default store;
+const pReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(pReducer, compose(
+  applyMiddleware(thunk, socketMiddleware(socketService))
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export const persistor = persistStore(store);
